87dfe626843a286bc42f0340ee5ece4747811a97
Ad450 <adjeimanue@gmail.com>
2022-02-16 14:10:43 +0000

    general election contract added

---
+++ contracts/general_election.sol
@@ -0,0 +1,93 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import "./electoral_commission.sol";
+
+/*   
+This contract will simulate a general election
+Voters will be validated and allowed to vote for 
+only one participating candidate.
+Voters will see the total votes of each candidate before 
+and after casting their votes
+
+
+*/
+
+contract GeneralElection is EC {
+
+  /*
+  @notice initialize all variables once the contract is deployed
+  @param [parties] defined in the EC interface
+  @param [voter] defined in the EC interface
+  @param [ballotBox] defined in the EC interface
+   */
+  constructor (string[] memory _parties){
+    parties = _parties;
+    voter = Voter({voterId: msg.sender, hasVoted: false, vote:0});
+    ballotBox = BallotBox({totalVotes: 0});
+  }
+
+  /* @notice [Voted] will be emitted into the chain */
+  event Voted(address from, string message);
+
+
+  /*
+     @notice will be replaced with the actual ballotBox object in any children
+     contract
+      */
+      BallotBox private ballotBox;
+
+
+    /* 
+    @notice for validation , will be replaced with the actual voter object 
+    wherever the modifier [validateVoter] will be called
+    */
+    Voter private voter;
+
+     /* 
+     @notice this validates all voters by checking for the address, hasVoted and vote
+      */
+   
+
+    /*
+    @notice gets the total votecast
+    @returns [ballotBox.totalVotes], the total votes in the box
+    @virtual can be overriden by a children contract
+     */
+     function getTotalVotes() public view override returns (uint256){
+       return ballotBox.totalVotes;
+     }
+
+    /*
+    @notice gets the total votes of a party
+    @param [_party] the party to get its votes
+    @returns [votecast[_party]] the number of votes of a party
+    @virtual can be overriden by a children contract
+    [validateParty] comes from the interface
+     */
+
+     function getPartyVotes(string memory _party) public view override returns(uint256){
+      // validate all parties
+        if(validateParty(_party)){
+          return votecast[_party];
+        }else{
+          revert("invalid party");
+        }
+     
+      }
+     
+
+     /*
+    @notice vote for a party
+    @param [_party] the party to vote for
+    @virtual can be overriden by a children contract'
+    @external cannot be called within this contract 
+     */
+
+     function voteForParty(string memory _party) external override validateVoter {
+       // increase the vote of the party by 1
+       votecast[_party] += 1;
+      emit Voted({from : voter.voterId, message:"vote recorded successfuly"});
+     }
+
+ 
+}
