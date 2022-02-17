b6b9cee8484f1893ea08d2430f81d32fdd5d637d
Ad450 <adjeimanue@gmail.com>
2022-02-17 14:13:32 +0000

    general lelection test is failing but works fine from remix

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,43 @@
-// import { expect } from "chai";
-// import { MockContract } from "ethereum-waffle";
-// import { ethers } from "hardhat";
+/* eslint-disable node/no-missing-import */
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { GeneralElection } from "../typechain/GeneralElection";
 
 // const address = 0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 // eslint-disable-next-line prettier/prettier
-describe("GeneralElection", () => {});
+describe("GeneralElection", () => {
+  let generalElection: GeneralElection;
+
+  beforeEach(async () => {
+    // const signers = await ethers.getSigners();
+    const GeneralElection = await ethers.getContractFactory("GeneralElection");
+
+    generalElection = await GeneralElection.deploy(["NPP", "NDC", "PPP"]);
+
+    (await generalElection).deployed();
+  });
+
+  it("should deploy contract successfuly", async () => {
+    console.log(`deployed at ${generalElection.address}`);
+  });
+
+  it("should return an empty ballot box || total votes should be 0", async () => {
+    // act
+    const result = await generalElection.getTotalVotes();
+
+    // assert
+    expect(result).equals(0);
+  });
+
+  it("should increase the votes of a party by 1 when a someone votes", async () => {
+    // act
+    await generalElection.voteForParty("PPP");
+    const result = await generalElection.getTotalVotes();
+    const partyResult = await generalElection.getPartyVotes("PPP");
+
+    // assert
+    expect(result).equals(1);
+    expect(partyResult).equals(1);
+  });
+});
